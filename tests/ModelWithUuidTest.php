46cd483cf0dff38d863890a4d3592b4c2ca3b3ab
Lorand Gombos <gombos.lorand@gmail.com>
2019-07-08 22:22:28 +0300

    Unit Test (#2)
    * Update Readme
    
    * Add UnitTest
    
    * Quick fix

--- tests/ModelWithUuidTest.php
+++ tests/ModelWithUuidTest.php
@@ -2,11 +2,23 @@
 
 namespace Glorand\LaravelEloquentModelUuid\Tests;
 
+use Glorand\LaravelEloquentModelUuid\Tests\Models\UserWithUuid;
+use Illuminate\Support\Str;
+
 class ModelWithUuidTest extends TestCase
 {
     /** @test */
-    public function testModel()
+    public function testModelWithUuid()
     {
-        $this->assertTrue(true);
+        /** @var UserWithUuid $testModel */
+        $testModel = UserWithUuid::query()->first();
+        $this->assertEquals(36, mb_strlen($testModel->id));
+
+        $uuid = Str::uuid()->toString();
+        /** @var UserWithUuid $testModel */
+        $testModel = UserWithUuid::query()->create([
+            'id' => $uuid,
+        ]);
+        $this->assertEquals($uuid, $testModel->id);
     }
 }
